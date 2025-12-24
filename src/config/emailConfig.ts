2979ea975b3680f6e6cf0df20decb56eabbef204
Josequesada.dev <josequesadajlq@gmail.com>
2025-12-24 17:05:48 -0300

    Backend terminado + deployment

--- src/config/emailConfig.ts
+++ src/config/emailConfig.ts
@@ -1,10 +1,13 @@
 import nodemailer from "nodemailer"
- 
+
+const USER = process.env.EMAIL_USER
+const PASS = process.env.EMAIL_PASS
+
 const transporter = nodemailer.createTransport({
-  service: "Gmail", 
+  service: "gmail",
   auth: {
-    user: process.env.EMAIL_USER,
-    pass: process.env.EMAIL_PASS
+    user: USER,
+    pass: PASS
   }
 })
 
--- src/services/emailService.ts
+++ src/services/emailService.ts
@@ -1,26 +1,28 @@
-// services/emailService.ts
+import { Request, Response } from "express"
 import transporter from "../config/emailConfig"
 import createTemplate from "../templates/emailTemplate"
 
-interface SendEmailParams {
-  subject: string
-  emailUser: string
-  message: string
+const emailService = async (req: Request, res: Response) => {
+  const { subject, email: emailUser, message } = req.body
+
+  if (!subject || !emailUser || !message) {
+    return res.status(400).json({ success: false, message: "Data invalida" })
+  }
+
+  try {
+    const info = await transporter.sendMail({
+      from: `Mensaje de la tienda: ${emailUser}`,
+      to: process.env.EMAIL_USER,
+      subject,
+      html: createTemplate(emailUser, message)
+    })
+
+    res.json({ succes: true, message: "Correo fue enviado exitosamente", info })
+
+  } catch (e) {
+    const error = e as Error
+    res.status(500).json({ success: false, error: error.message })
+  }
 }
 
-const sendWelcomeEmail = async ({
-  subject,
-  emailUser,
-  message
-}: SendEmailParams): Promise<void> => {
-
-  await transporter.sendMail({
-    from: `"GamerStore" <${process.env.EMAIL_USER}>`,
-    to: emailUser,
-    subject,
-    html: createTemplate(emailUser, message)
-  })
-}
-
-export default sendWelcomeEmail
-
+export default emailService
